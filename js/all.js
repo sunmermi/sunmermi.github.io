4a732c7f3b16e8fa85e5a538d7d32e4c940287e2
sseom <ysum1011@gmail.com>
2017-01-17 12:21:19 +0900

    포폴 어바웃 버튼 클릭 JS 3번째 방법으로 수정, 어바웃버튼 클릭 후 나오는 포폴 소개 콘텐츠가 활성화 되었을 때 또 다른 어바웃 클릭시 이전 콘텐츠는 닫힘

--- js/all.js
+++ js/all.js
@@ -48,11 +48,114 @@
     + 결국은 slide-up, slide-down 클래스를 추가 제거하는 방법인데 
         * display: none, display: block 안쓰고 
         * 클래스를 붙여서 max-height 값을 조정
+
+// -------------- CSS ----------------
+    // 버튼 슬라이드 이벤트
+    .slide-up, .slide-down {
+      max-height: 0;
+      overflow-y: hidden;
+      -webkit-transition: max-height 0.5s ease-in-out;
+      -moz-transition: max-height 0.5s ease-in-out;
+      -o-transition: max-height 0.5s ease-in-out;
+      transition: max-height 0.5s ease-in-out;
+    }
+    .slide-down {
+       max-height: 800px; 
+    }
  * -------------------------------------
  */
 
-(function(global) {
-  "use strict";
+// (function(global) {
+//   "use strict";
+//   // 대상선택
+//   var about_content  = document.querySelectorAll(".about_content");
+//   var about_btns  = document.querySelectorAll('.about_btn');
+//   var about_btns_length = about_btns.length;
+//   var about_close_btn = document.querySelectorAll('.about_close_btn');
+
+//   //반복 순환 : 어바웃버튼, 콘텐츠, 클로즈 버튼 모두 여러개니까...
+//   for(var i =0; i < about_btns_length; i++){
+
+//     //어바웃 버튼
+//     var about_btn = about_btns[i];
+//     about_btn.num = i;
+//     about_btn.onclick = function(){
+//       // console.log(this);
+//       // console.log(this.num);
+//       classToggle(this.num);
+//       return false;
+//     };
+
+//     //클로즈 버튼
+//     var about_close = about_close_btn[i];
+//     about_close.num = i;
+//     about_close.onclick = function(){
+//       classToggle(this.num);
+//       return false;
+//     };
+
+//     //라망 작업일지 버튼 비활성화
+//     // var job_btn=document.querySelectorAll('.job_btn');
+//     // job_btn[0].onclick = function(){
+//     //   alert('La-main 작업일지는 아직 업로드 되지 않았습니다.');
+//     //   return false;
+//     // };
+//   }
+
+
+//   // 클래스 투글 함수 ( 어바웃버튼 클릭 오픈 또 클릭하면 클로즈 )
+//   // 인자 index는 클릭한 대상의 자신의 인덱스 값
+//   function classToggle(index){
+//     // 슬라이드 다운, 업 클래스 변수
+//     var remove_class    = "slide-down";
+//     var add_class       = "slide-up";
+
+//     // 객체.className ==> 해당 포폴소개 콘텐츠의 클래스 네임값
+//     // .replace() ==> 줄바꿈, 탭문자, 리턴문자 모두 찹아서 없애라 비워. 공백으로..
+//     var element_classes = ( about_content[index].className ).replace(/[\n\t\r]/g, " ");
+//     // console.log(element_classes);
+//     //=> 포폴소개 콘텐츠의 클래스 네임값이 할당됨.
+
+//     // 상태변수
+//     // 포폴소개 콘텐츠에 slide-down의 인덱스값이 즉, 9인데,, -1 보다 크면 트루
+//     // 즉, slide-down이 있으면 트루 slide-down이 없으면 펄스
+//     // 그냥 다운이 있냐 없냐의 유무  
+//     var is_showing      = element_classes.indexOf(remove_class) > -1;
+//     // console.log(is_showing);
+
+//     // is_showing 이 flase 라면 즉, slide-down 클래스가 없으면
+//     if ( ! is_showing) {
+//         // 변수값 전환
+//         // 리무브에 slide-up 클래스를 할당해라.
+//         remove_class = [add_class, add_class = remove_class][0];
+//     }
+
+//     // 이전클래스를 제거하고 새로운 클래스 추가
+//     // about_content[index] ==> 클릭한 버튼에 해당되는 포폴소개 콘텐츠
+//     // 콘텐츠의 클래스네임 remove_class 를 지우고 
+//     // add_class 클래스를 추가 해
+//     // trim() 은 단어의 앞,뒤 공백 없애줌
+//     about_content[index].className = (element_classes.replace(remove_class, "") + add_class ).trim();
+//     // console.log(add_class);
+//     // console.log(remove_class);
+//     //==> add_class, remove_class 변수의 값은 서로가 계속 바뀜
+//   }
+
+// })(this);
+
+
+/**
+ * -------------------------------------
+ * - 세번째 방법
+    + 어차피 클래스 추가. 제거 하는 방식이면,,,, 객체.className.replace()말고
+    + 내가 아는 객체.classList.add() , 객체.classList.remove() 이용해서 해보기,,,,
+ * -------------------------------------
+ */
+
+
+ (function(global){
+   'use strict';
+   
   // 대상선택
   var about_content  = document.querySelectorAll(".about_content");
   var about_btns  = document.querySelectorAll('.about_btn');
@@ -68,7 +171,8 @@
     about_btn.onclick = function(){
       // console.log(this);
       // console.log(this.num);
-      classToggle(this.num);
+      hasClassRmove( about_content , 'slide-down');
+      slideClass(about_content, this.num);
       return false;
     };
 
@@ -76,65 +180,58 @@
     var about_close = about_close_btn[i];
     about_close.num = i;
     about_close.onclick = function(){
-      classToggle(this.num);
+      slideClass(about_content, this.num);
       return false;
     };
 
-    //라망 작업일지 버튼 비활성화
-    // var job_btn=document.querySelectorAll('.job_btn');
-    // job_btn[0].onclick = function(){
-    //   alert('La-main 작업일지는 아직 업로드 되지 않았습니다.');
-    //   return false;
-    // };
   }
 
-
-  // 클래스 투글 함수
-  // 인자 index는 클릭한 대상의 자신의 인덱스 값
-  function classToggle(index){
-    // 슬라이드 다운, 업 클래스 변수
-    var remove_class    = "slide-down";
-    var add_class       = "slide-up";
-
-    // 객체.className ==> 해당 포폴소개 콘텐츠의 클래스 네임값
-    // .replace() ==> 줄바꿈, 탭문자, 리턴문자 모두 찹아서 없애라 비워. 공백으로..
-    var element_classes = ( about_content[index].className ).replace(/[\n\t\r]/g, " ");
-    // console.log(element_classes);
-    //=> 포폴소개 콘텐츠의 클래스 네임값이 할당됨.
-
-    // 상태변수
-    // 포폴소개 콘텐츠에 slide-down의 인덱스값이 즉, 9인데,, -1 보다 크면 트루
-    // 즉, slide-down이 있으면 트루 slide-down이 없으면 펄스
-    // 그냥 다운이 있냐 없냐의 유무  
-    var is_showing      = element_classes.indexOf(remove_class) > -1;
-    // console.log(is_showing);
-
-    // is_showing 이 flase 라면 즉, slide-down 클래스가 없으면
-    if ( ! is_showing) {
-        // 변수값 전환
-        // 리무브에 slide-up 클래스를 할당해라.
-        remove_class = [add_class, add_class = remove_class][0];
+  // 슬라이드 클래스
+  function slideClass(content, idx) {
+    // 다운클래스가 있다면
+    // 다운클래스를 삭제 remove 슬라이드업
+    // 없다면 추가 add 슬라이드다운
+    var is = content[idx].classList.contains('slide-down');
+    if( !is ){
+      content[idx].classList.add('slide-down');
+    }else{
+      content[idx].classList.remove('slide-down');
     }
-
-    // 이전클래스를 제거하고 새로운 클래스 추가
-    // about_content[index] ==> 클릭한 버튼에 해당되는 포폴소개 콘텐츠
-    // 콘텐츠의 클래스네임 remove_class 를 지우고 
-    // add_class 클래스를 추가 해
-    // trim() 은 단어의 앞,뒤 공백 없애줌
-    about_content[index].className = (element_classes.replace(remove_class, "") + add_class ).trim();
-    // console.log(add_class);
-    // console.log(remove_class);
-    //==> add_class, remove_class 변수의 값은 서로가 계속 바뀜
   }
+
+
+  // 요소에 클래스 있는지 없는지 확인후 있으면 삭제
+  function hasClassRmove(arrNode, className){
+    for(var i =0 , l = arrNode.length ; i < l ; i++){
+      // console.log('ok');
+      var aa = arrNode[i];
+      // console.log(aa);
+      var bb = aa.classList.contains(className);
+      // console.log(bb);
+      if(bb){
+          // return console.log(aa);
+        return aa.classList.remove(className);
+      }
+    }
+  }
+
 
 })(this);
 
 
-/**
- * -------------------------------------
- * - 세번째 방법
-    + 어차피 클래스 추가. 제거 하는 방식이면,,,, 객체.className.replace()말고
-    + 내가 아는 객체.classList.add() , 객체.classList.remove() 이용해서 해보기,,,,
- * -------------------------------------
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
